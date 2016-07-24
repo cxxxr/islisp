fb5bdc3fd152e67880f1663c24b4c95c7e270f76
cxxxr <g23tlm@gmail.com>
2016-07-24 17:14:17 +0900

    ベクタ操作関数の修正

--- runtime/vector.c
+++ runtime/vector.c
@@ -2,18 +2,12 @@
 
 void is_create_vector_2f(void)
 {
-	ISObject *num = is_stack_peek_ptr(2);
+	ISObject len = is_stack_peek(2);
 	ISObject *v = is_stack_peek_ptr(1);
-
-	int len;
-	if (IS_INTEGER_P(*num)) {
-		len = IS_INTEGER(num);
-	} else if (IS_FLOAT_P(*num)) {
-		len = IS_FLOAT_VALUE(num);
-	} else {
-		is_type_error(*num, IS_NUMBER_TYPE);
+	if (!IS_INTEGER_P(len)) {
+		is_type_error(len, IS_NUMBER_TYPE);
 	}
-	is_stack_drop_push(2, is_make_vector(len, v));
+	is_stack_drop_push(2, is_make_vector(IS_INTEGER(len), v));
 }
 
 void is_create_vector_f(int argc)
@@ -41,16 +35,16 @@ void is_vector_ref_f(int IS_UNUSED(argc))
 
 void is_vector_set_f(int IS_UNUSED(argc))
 {
-	ISObject *vec = is_stack_peek_ptr(3);
-	ISObject index = is_stack_peek(2);
-	ISObject *val = is_stack_peek_ptr(1);
+	ISObject *val = is_stack_peek_ptr(3);
+	ISObject *vec = is_stack_peek_ptr(2);
+	ISObject index = is_stack_peek(1);
 
 	if (!IS_VECTOR_P(*vec)) is_type_error(*vec, IS_VECTOR_TYPE);
 	if (!IS_INTEGER_P(index)) is_type_error(index, IS_INTEGER_TYPE);
 
 	int i = IS_INTEGER(index);
 
-	if (i < 0 || IS_VECTOR_LENGTH(vec) <= i)
+	if (i < 0 || IS_VECTOR_LENGTH(*vec) <= i)
 		is_index_out_of_range(*vec, index);
 
 	// write_barrior
