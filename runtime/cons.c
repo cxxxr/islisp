fcff893d8819a94e39caec5b0836e53efcc4c8cd
cxxxr <g23tlm@gmail.com>
2016-07-24 20:27:57 +0900

    修正

--- runtime/cons.c
+++ runtime/cons.c
@@ -10,7 +10,7 @@ void is_cons_f(int IS_UNUSED(argc))
 void is_car_f(int IS_UNUSED(argc))
 {
 	ISObject *obj = is_stack_peek_ptr(1);
-	if (!IS_CONS_P(obj))
+	if (!IS_CONS_P(*obj))
 		is_type_error(*obj, IS_CONS_TYPE);
 	is_stack_change_tos(IS_CONS_CAR(*obj));
 }
@@ -18,7 +18,7 @@ void is_car_f(int IS_UNUSED(argc))
 void is_cdr_f(int IS_UNUSED(argc))
 {
 	ISObject *obj = is_stack_peek_ptr(1);
-	if (!IS_CONS_P(obj))
+	if (!IS_CONS_P(*obj))
 		is_type_error(*obj, IS_CONS_TYPE);
 	is_stack_change_tos(IS_CONS_CDR(*obj));
 }
